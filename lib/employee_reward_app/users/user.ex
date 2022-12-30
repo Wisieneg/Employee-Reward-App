de9f54e303637761a814b32413f214235cee8a8b
Wisieneg <wojtas.w1@op.pl>
2022-12-30 17:52:34 +0100

    relation fix for user - reward schemas

--- lib/employee_reward_app/users/user.ex
+++ lib/employee_reward_app/users/user.ex
@@ -9,10 +9,19 @@ defmodule EmployeeRewardApp.Users.User do
     field :role, :string
     field :points, :integer
 
+    has_many :given_to, EmployeeRewardApp.Rewards.Reward, foreign_key: :to_id
+    has_many :given_by, EmployeeRewardApp.Rewards.Reward, foreign_key: :from_id
+
     pow_user_fields()
 
     timestamps()
 
+  end
+
+
+  def update_changeset(user_or_changeset, attrs) do
+    user_or_changeset
+    |> cast(attrs, [:role, :points, :username])
   end
 
   def changeset(user_or_changeset, attrs) do
@@ -21,6 +30,7 @@ defmodule EmployeeRewardApp.Users.User do
     |> cast(attrs, [:username, :name])
     |> validate_required([:username, :name])
     |> validate_length(:username, min: 3, max: 40)
+    |> unique_constraint(:username)
   end
 
 end
