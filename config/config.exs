753795b123996b175dce539d8f1e65908cb17a9a
Wisieneg <wojtas.w1@op.pl>
2022-12-30 12:41:43 +0100

    basic config for Pow library

--- config/config.exs
+++ config/config.exs
@@ -44,6 +44,10 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :employee_reward_app, :pow,
+  user: EmployeeRewardApp.Users.User,
+  repo: EmployeeRewardApp.Repo
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
--- lib/employee_reward_app_web/router.ex
+++ lib/employee_reward_app_web/router.ex
@@ -1,5 +1,6 @@
 defmodule EmployeeRewardAppWeb.Router do
   use EmployeeRewardAppWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -12,6 +13,12 @@ defmodule EmployeeRewardAppWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    pow_routes()
   end
 
   scope "/", EmployeeRewardAppWeb do
