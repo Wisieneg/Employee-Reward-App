43a46793bd2661a690f24db61366d695fbab14ef
Wisieneg <wojtas.w1@op.pl>
2022-12-30 16:40:45 +0100

    html redirects in templates

--- lib/employee_reward_app_web/views/layout_view.ex
+++ lib/employee_reward_app_web/views/layout_view.ex
@@ -4,4 +4,10 @@ defmodule EmployeeRewardAppWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def first_name(user) do
+    user.name
+    |> String.split()
+    |> Enum.at(0)
+  end
 end
--- lib/employee_reward_app_web/views/user_view.ex
+++ lib/employee_reward_app_web/views/user_view.ex
@@ -1,3 +1,9 @@
 defmodule EmployeeRewardAppWeb.UserView do
   use EmployeeRewardAppWeb, :view
+
+  def first_name(user) do
+    user.name
+    |> String.split()
+    |> Enum.at(0)
+  end
 end
