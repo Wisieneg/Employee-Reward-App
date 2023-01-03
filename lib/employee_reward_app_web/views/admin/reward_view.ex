c5c85201d8d01cdaadba04ce40aaa0f0be4ae031
Wisieneg <wojtas.w1@op.pl>
2023-01-03 23:35:57 +0100

    reward view functions

--- lib/employee_reward_app_web/views/admin/reward_view.ex
+++ lib/employee_reward_app_web/views/admin/reward_view.ex
@@ -12,4 +12,33 @@ defmodule EmployeeRewardAppWeb.Admin.RewardView do
 
   def format_time(date_time), do: "#{Time.to_string(date_time)}"
 
+  def format_datetime(date_time) do
+    {:ok, formatted} = Timex.format(date_time, "%H:%M %d/%m/%y", :strftime)
+    formatted
+  end
+
+  def summary_date(month) do
+    date = Timex.parse!(month, "{YYYY}-{0M}")
+
+    Calendar.strftime(
+      date,
+      "%B %Y",
+      month_names: fn month ->
+        {
+         "January",
+         "February",
+         "March",
+         "April",
+         "May",
+         "June",
+         "July",
+         "August",
+         "September",
+         "October",
+         "November",
+         "December"
+         } |> elem(month-1) end
+    )
+  end
+
 end
--- lib/employee_reward_app_web/views/reward_view.ex
+++ lib/employee_reward_app_web/views/reward_view.ex
@@ -1,3 +1,21 @@
 defmodule EmployeeRewardAppWeb.RewardView do
   use EmployeeRewardAppWeb, :view
+
+  alias EmployeeRewardApp.Users
+
+  def get_user_points(user) do
+    %{points: points} = Users.get_user!(user.id)
+    points
+  end
+
+  def get_username(user_id) do
+    user = Users.get_user!(user_id)
+    user.username
+  end
+
+  def format_date(date_time), do: "#{Date.to_string(date_time)}"
+
+  def format_time(date_time), do: "#{Time.to_string(date_time)}"
+
+  def format_datetime(date_time), do: "#{format_date(date_time)} #{format_time(date_time)}"
 end
