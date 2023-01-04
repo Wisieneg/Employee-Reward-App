2dd32002d4850a3c505fd5d7f21ba12825620b7d
Wisieneg <wojtas.w1@op.pl>
2023-01-04 13:33:23 +0100

    templates and view for user rewards

--- lib/employee_reward_app_web/views/reward_view.ex
+++ lib/employee_reward_app_web/views/reward_view.ex
@@ -3,19 +3,19 @@ defmodule EmployeeRewardAppWeb.RewardView do
 
   alias EmployeeRewardApp.Users
 
+  # The points are received from the view in case they were modified.
   def get_user_points(user) do
     %{points: points} = Users.get_user!(user.id)
     points
-  end
-
-  def get_username(user_id) do
-    user = Users.get_user!(user_id)
-    user.username
   end
 
   def format_date(date_time), do: "#{Date.to_string(date_time)}"
 
   def format_time(date_time), do: "#{Time.to_string(date_time)}"
 
-  def format_datetime(date_time), do: "#{format_date(date_time)} #{format_time(date_time)}"
+  def format_datetime(date_time) do
+    {:ok, formatted} = Timex.format(date_time, "%d/%m/%Y, %H:%M", :strftime)
+    formatted
+  end
+
 end
